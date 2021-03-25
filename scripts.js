46e1343f4ee4645f45bb803aabce356c35d49d75
Al Hounsell <al@Als-MacBook-Pro.local>
2021-03-25 16:34:56 -0400

    code cleanup

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
         var savedListJSON = localStorage.getItem("savedLists");
         if(savedListJSON) savedLists = JSON.parse(savedListJSON);
         for(let key in savedLists){
-        $("#existing-lists").append(`<option value="${key}">${key}</option>`);
+            $("#existing-lists").append(`<option value="${key}">${key}</option>`);
         }
     } else {
         // Sorry! No Web Storage support..
@@ -29,7 +29,6 @@ function loadList(){
     var listName = $("#existing-lists").val();
     var list = savedLists[listName];
     $("#list").val(list);
-    //buildList();
 }
 
 function deleteList(){
@@ -44,15 +43,6 @@ function deleteList(){
 }
 
 var synth = window.speechSynthesis;
-
-var inputForm = document.querySelector('form');
-var inputTxt = document.querySelector('.txt');
-var voiceSelect = document.querySelector('select');
-
-var pitch = document.querySelector('#pitch');
-var pitchValue = document.querySelector('.pitch-value');
-var rate = document.querySelector('#rate');
-var rateValue = document.querySelector('.rate-value');
 
 var voices = [];
 
@@ -120,62 +110,21 @@ function check(){
     }
 }
 
-function populateVoiceList() {
-    voices = synth.getVoices().sort(function (a, b) {
-        const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
-        if ( aname < bname ) return -1;
-        else if ( aname == bname ) return 0;
-        else return +1;
-    });
-    var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-    voiceSelect.innerHTML = '';
-    for(i = 0; i < voices.length ; i++) {
-        var option = document.createElement('option');
-        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-        
-        if(voices[i].default) {
-            option.textContent += ' -- DEFAULT';
-        }
-
-        option.setAttribute('data-lang', voices[i].lang);
-        option.setAttribute('data-name', voices[i].name);
-        voiceSelect.appendChild(option);
-    }
-    voiceSelect.selectedIndex = selectedIndex;
-}
-
-//populateVoiceList();
-// if (speechSynthesis.onvoiceschanged !== undefined) {
-//   speechSynthesis.onvoiceschanged = populateVoiceList;
-// }
-
 function howSpell(text){
     say("How do you spell: " + text);
 }
 
 function say(text){
     
-    // if (synth.speaking) {
-    //     console.error('speechSynthesis.speaking');
-    //     return;
-    // }
     if (text !== '') {
         var utterThis = new SpeechSynthesisUtterance(text);
         utterThis.onend = function (event) {
-            console.log('SpeechSynthesisUtterance.onend');
+            //console.log('SpeechSynthesisUtterance.onend');
         }
         utterThis.onerror = function (event) {
             console.error('SpeechSynthesisUtterance.onerror');
         }
-        // var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-        // for(i = 0; i < voices.length ; i++) {
-        //   if(voices[i].name === selectedOption) {
-        //     utterThis.voice = voices[i];
-        //     break;
-        //   }
-        // }
-        // utterThis.pitch = 1;
-        // utterThis.rate = 1;
+
         synth.speak(utterThis);
     }
 }
